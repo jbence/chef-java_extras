28c4f8691c245d14bea0b847986f9785a0649e90
James Bence <jbence@mdsol.com>
2014-09-29 14:00:36 -0700

    Add leiningen recipe to install leiningen tool

--- recipes/leiningen.rb
+++ recipes/leiningen.rb
@@ -1,5 +1,6 @@
 #
-# Cookbook Name:: java_extras
+# Cookbook Name:: chef-java_extras
+# Recipe:: leiningen
 #
 # Copyright 2014, Medidata Solutions, Inc.
 #
@@ -14,9 +15,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-default['java']['jce_policy']['7']['url'] = 'https://s3.amazonaws.com/medidata-installs/unlimited_jce_policy-7.zip'
-default['java']['jce_policy']['7']['checksum'] = '7a8d790e7bd9c2f82a83baddfae765797a4a56ea603c9150c87b7cdb7800194d'
-default['leiningen']['version'] = '2.4.0'
-default['leiningen']['lein_install_dir'] = '/usr/local/bin'
+lein_install_path = "#{node['leiningen']['lein_install_dir']}/lein"
+lein_version = node['leiningen']['version']
+
+remote_file lein_install_path do
+  source "https://raw.github.com/technomancy/leiningen/#{lein_version}/bin/lein"
+  owner "root"
+  group "root"
+  mode 0755
+  not_if "grep -qx 'export LEIN_VERSION=\"#{lein_version}\"' #{lein_install_path}"
+end
